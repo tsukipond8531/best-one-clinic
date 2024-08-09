4717eac4828e78c24249210dba7796d512818ff4
tsukipond8531 <reader2027@gmail.com>
2024-08-09 18:05:33 +0900

    update i18n(set English as first languages)

--- src/Components/Translation/i18n.jsx
+++ src/Components/Translation/i18n.jsx
@@ -1,29 +1,29 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import TranslationEn  from "./English.json"
-import TranslationAr  from "./Arabic.json"
+import TranslationEn from "./English.json"
+import TranslationAr from "./Arabic.json"
 import languageDectector from "i18next-browser-languagedetector"
 
-const resources ={
-    en : {
-        translation : TranslationEn
+const resources = {
+    en: {
+        translation: TranslationEn
     },
-    ar:{
-        translation : TranslationAr
+    ar: {
+        translation: TranslationAr
     }
 }
 
-i18n 
+i18n
     .use(languageDectector)
     .use(initReactI18next)
     .init({
-        resources , 
-        lng : 'ar',
-        interpolation : {
-            escapeValue : false
+        resources,
+        lng: 'en',
+        interpolation: {
+            escapeValue: false
         },
-        react : {
-            useSuspense : false
+        react: {
+            useSuspense: false
         }
     })
 
